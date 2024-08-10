469f1db3023851b3e4e063d6f26b2593dfdcf160
LucasTavares77 <lkazx777@gmail.com>
2024-08-09 21:54:55 -0300

    Primeira pagina web

---
+++ configura-git.bat
@@ -0,0 +1,12 @@
+echo OFF
+
+echo --- Configurando Usuario GitHub ----
+
+git config --global user.name "LucasTavares77"
+git config --global user.email "lkazx777@gmail.com"
+echo --- mostrando a configuração atual do Github no computador
+git config config --list
+echo --- Fim ---
+echo --- Ingrid Alves nao quer derrubar o governo ---
+
+pause
